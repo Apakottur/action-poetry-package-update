29e59c8b9915a40036006f27feab2b298b30744f
Yossi Rozantsev <54272821+Apakottur@users.noreply.github.com>
2022-07-21 13:12:08 +0000

    Fix incorrect run order when using path dependencies (#5)

--- src/updater.py
+++ src/updater.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import functools
 import os
 from pathlib import Path
 
@@ -9,13 +10,19 @@ import tomlkit.exceptions
 """Poetry configuration file name"""
 POETRY_CONFIG_FILE_NAME = "pyproject.toml"
 
+"""Sections in the Poetry configuration files where dependencies reside"""
+SECTIONS = ("dependencies", "dev-dependencies")
+
 
 def _run_updater_in_path(path: str) -> None:
     """
     Run the updater in the specified path.
     """
-    # Iterate over all files in the directory
-    for root, _dirs, files in os.walk(path, topdown=False):
+    # Mapping from projects to their path dependencies.
+    file_path_to_deps: dict[Path, list[Path]] = {}
+
+    # First iteration - find all projects and create a mapping from projects to their path dependencies.
+    for root, _dirs, files in os.walk(path):
         for name in files:
             # Skip non Poetry configuration files.
             if name != POETRY_CONFIG_FILE_NAME:
@@ -26,58 +33,81 @@ def _run_updater_in_path(path: str) -> None:
             file_contents = open(file_path).read()
             parsed_contents = tomlkit.parse(file_contents)
 
-            print(f"TOML contents of {file_path}: {parsed_contents}")
-
-            # Get the poetry configuration.
+            # Get the poetry configuration, skipping if there is none.
             try:
                 poetry_section = parsed_contents["tool"]["poetry"]
             except tomlkit.exceptions.NonExistentKey:
-                # Skip if there is no poetry section.
                 continue
 
-            # Update the lock file, creating it if needed.
-            shpyx.run("poetry update --lock", exec_dir=root)
+            # Build the mapping from projects to their path dependencies.
+            file_path_to_deps[file_path] = []
+            for section in SECTIONS:
+                try:
+                    poetry_section = poetry_section[section]
+                except tomlkit.exceptions.NonExistentKey:
+                    continue
 
-            # Get all the outdated packages.
-            results = shpyx.run("poetry show -o --no-ansi", exec_dir=root)
+                for details in poetry_section.values():
+                    if "path" in details:
+                        file_path_to_deps[file_path].append((Path(root) / details["path"] / name).resolve())
 
-            if results.stdout == "":
-                # Nothing to update.
-                continue
+    # Order the projects based on interdependencies, where dependencies go first.
+    file_paths_in_order = sorted(
+        file_path_to_deps, key=functools.cmp_to_key(lambda x, y: 1 if y in file_path_to_deps[x] else -1)
+    )
 
-            # Update the file contents, for each outdated package.
-            for result in results.stdout.strip().split("\n"):
-                # Remove the "(!)" decoration used to mark packages as non installed.
-                result = result.replace(" (!) ", " ")
+    # Second iteration - run updates in order.
+    for file_path in file_paths_in_order:
+        # Get the contents of the configuration file.
+        file_contents = open(file_path).read()
+        parsed_contents = tomlkit.parse(file_contents)
+        poetry_section = parsed_contents["tool"]["poetry"]
 
-                # Get the package details.
-                package_name, installed_version, new_version = result.split()[:3]
+        print(f"TOML contents of {file_path}: {parsed_contents}")
 
-                # Update the package version in the file.
-                for section in ("dependencies", "dev-dependencies"):
-                    try:
-                        original_package_name, package_details = next(
-                            (name, details)
-                            for name, details in poetry_section[section].items()
-                            if name.lower() == package_name
-                        )
-                    except (StopIteration, tomlkit.exceptions.NonExistentKey):
-                        # Either the section is missing or the package is not in this section.
-                        continue
+        # Update the lock file, creating it if needed.
+        shpyx.run("poetry update --lock", exec_dir=file_path.parent)
 
-                    print(f"Updating {package_name}: {installed_version} -> {new_version}")
+        # Get all the outdated packages.
+        results = shpyx.run("poetry show -o --no-ansi", exec_dir=file_path.parent)
 
-                    # Replace the old version of the package with the new one.
-                    if isinstance(package_details, str):
-                        poetry_section[section][original_package_name] = new_version
-                    else:
-                        poetry_section[section][original_package_name]["version"] = new_version
+        if results.stdout == "":
+            # Nothing to update.
+            continue
 
-            # Write the updated configuration file.
-            open(file_path, "w").write(parsed_contents.as_string())
+        # Update the file contents, for each outdated package.
+        for result in results.stdout.strip().split("\n"):
+            # Remove the "(!)" decoration used to mark packages as non installed.
+            result = result.replace(" (!) ", " ")
 
-            # Finally, regenerate the lock file again, with the new package versions.
-            shpyx.run("poetry update --lock", exec_dir=root)
+            # Get the package details.
+            package_name, installed_version, new_version = result.split()[:3]
+
+            # Update the package version in the file.
+            for section in SECTIONS:
+                try:
+                    original_package_name, package_details = next(
+                        (name, details)
+                        for name, details in poetry_section[section].items()
+                        if name.lower() == package_name
+                    )
+                except (StopIteration, tomlkit.exceptions.NonExistentKey):
+                    # Either the section is missing or the package is not in this section.
+                    continue
+
+                print(f"Updating {package_name}: {installed_version} -> {new_version}")
+
+                # Replace the old version of the package with the new one.
+                if isinstance(package_details, str):
+                    poetry_section[section][original_package_name] = new_version
+                else:
+                    poetry_section[section][original_package_name]["version"] = new_version
+
+        # Write the updated configuration file.
+        open(file_path, "w").write(parsed_contents.as_string())
+
+        # Finally, regenerate the lock file again, with the new package versions.
+        shpyx.run("poetry update --lock", exec_dir=file_path.parent)
 
 
 def run_updater(paths: list[str]):
--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,174 +1,275 @@
 import tempfile
+from dataclasses import dataclass
 from pathlib import Path
 
+import updater
+from pytest_mock import MockerFixture
 from updater import POETRY_CONFIG_FILE_NAME, run_updater
 
 
-def _run_updater(before: str, after: str) -> None:
+@dataclass
+class Project:
     """
-    Run the update and verify the result.
+    Poetry project configuration.
+    """
+
+    # The contents of the TOML file before the update.
+    before: str
+
+    # The contents of the TOML file after the update.
+    after: str
+
+    # The relative path to the project file.
+    path: str = ""
+
+
+def _run_updater(projects: list[Project], *, add_tool_poetry: bool = True, tmp_dir: str | None = None) -> None:
+    """
+    Run the updater and verify the result.
 
     Args:
-        before: Contents of the Poetry config file before the update.
-        after: Contents of the Poetry config file after the update.
+        projects: Poetry project details
+        add_tool_poetry: Whether to add a "tool.poetry" clause to the files.
+        tmp_dir: Temporary directory to use. If None, one is created.
     """
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # Create the file.
-        file_path = Path(tmp_dir) / POETRY_CONFIG_FILE_NAME
-        open(file_path, "w").write(before)
+    # Prepare file details.
+    for project in projects:
+        project_name = Path(project.path).name
+
+        # Add "tool.poetry" section if needed.
+        if add_tool_poetry:
+            project.before = f"""
+                    [tool.poetry]
+                    name = "{project_name}"
+                    version = "1.0.0"
+                    description = ""
+                    authors = []
+
+                    {project.before}
+                    """
+            project.after = f"""
+                    [tool.poetry]
+                    name = "{project_name}"
+                    version = "1.0.0"
+                    description = ""
+                    authors = []
+
+                    {project.after}
+                    """
+
+        # Normalize the lines.
+        project.before = "\n".join(line.strip() for line in project.before.split("\n"))
+        project.after = "\n".join(line.strip() for line in project.after.split("\n"))
+
+    def run_in_tmp_directory():
+        # Create the files
+        for p in projects:
+            file_path = Path(tmp_dir) / p.path / POETRY_CONFIG_FILE_NAME
+            Path.mkdir(file_path.parent, exist_ok=True)
+            open(file_path, "w").write(p.before)
 
         # Run the updater.
         run_updater([tmp_dir])
 
         # Compare the files.
-        assert after == open(file_path).read()
+        for p in projects:
+            file_path = Path(tmp_dir) / p.path / POETRY_CONFIG_FILE_NAME
+            assert p.after == open(file_path).read()
+
+    # Create a temporary directory or use an existing run and then run the updater.
+    if tmp_dir:
+        run_in_tmp_directory()
+    else:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            run_in_tmp_directory()
 
 
 def test_poetry_deps():
     """A package under `dependencies` is out of date"""
     _run_updater(
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        shpyx = "0.0.13"
-        """,
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        shpyx = "0.0.14"
-        """,
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.13"
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.14"
+                """,
+            )
+        ]
     )
 
 
 def test_poetry_dev_deps():
     """A package under `dev-dependencies` is out of date"""
     _run_updater(
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
 
-        [tool.poetry.dependencies]
-        python = "3.10"
+                [tool.poetry.dev-dependencies]
+                shpyx = "0.0.13"
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
 
-        [tool.poetry.dev-dependencies]
-        shpyx = "0.0.13"
-        """,
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-
-        [tool.poetry.dev-dependencies]
-        shpyx = "0.0.14"
-        """,
+                [tool.poetry.dev-dependencies]
+                shpyx = "0.0.14"
+                """,
+            )
+        ]
     )
 
 
 def test_multiline_deps():
     """A package with a multi-line configuration is out of date"""
     _run_updater(
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        sqlalchemy = { extras = [
-          "postgresql",
-          "postgresql_asyncpg"
-        ], version = "1.4.36" }
-        """,
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        sqlalchemy = { extras = [
-          "postgresql",
-          "postgresql_asyncpg"
-        ], version = "1.4.37" }
-        """,
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                sqlalchemy = { extras = [
+                  "postgresql",
+                  "postgresql_asyncpg"
+                ], version = "1.4.36" }
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                sqlalchemy = { extras = [
+                  "postgresql",
+                  "postgresql_asyncpg"
+                ], version = "1.4.39" }
+                """,
+            )
+        ]
     )
 
 
 def test_no_changes():
     """Everything is up to date"""
     _run_updater(
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        shpyx = "0.0.14"
-        """,
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        shpyx = "0.0.14"
-        """,
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.14"
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.14"
+                """,
+            )
+        ]
     )
 
 
 def test_casing():
     """Verify that lower/upper case in package names is preserved"""
     _run_updater(
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        sHpYx = "0.0.13"
-        """,
-        """
-        [tool.poetry]
-        name = "test"
-        version = "1.0.0"
-        description = ""
-        authors = []
-
-        [tool.poetry.dependencies]
-        python = "3.10"
-        sHpYx = "0.0.14"
-        """,
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                sHpYx = "0.0.13"
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                sHpYx = "0.0.14"
+                """,
+            )
+        ]
     )
+
+
+def test_path_dependency_run_order(mocker: MockerFixture):
+    """Verify that the update order is correct when using path dependencies"""
+    # Track calls to "shpyx.run".
+    shpyx_run_spy = mocker.spy(updater.shpyx, "run")
+
+    tmp_dir = tempfile.TemporaryDirectory()
+
+    # Make sure the results of `os.walk` are NOT in the correct update order.
+    def _os_walk(path: str, *args, **kwargs):
+        assert (path, args, kwargs) == (tmp_dir.name, (), {})
+        return [
+            (tmp_dir, ["outer", "inner_1", "inner_2"], []),
+            (f"{tmp_dir.name}/outer", ["outer"], [POETRY_CONFIG_FILE_NAME]),
+            (f"{tmp_dir.name}/inner_1", ["inner_1"], [POETRY_CONFIG_FILE_NAME]),
+            (f"{tmp_dir.name}/inner_2", ["inner_2"], [POETRY_CONFIG_FILE_NAME]),
+        ]
+
+    mocker.patch("updater.os.walk", _os_walk)
+
+    _run_updater(
+        [
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                inner_1 = { path = "../inner_1", develop = true }
+                inner_2 = { path = "../inner_2", develop = false }
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                inner_1 = { path = "../inner_1", develop = true }
+                inner_2 = { path = "../inner_2", develop = false }
+                """,
+                "outer",
+            ),
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                inner_2 = { path = "../inner_2", develop = false }
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                inner_2 = { path = "../inner_2", develop = false }
+                """,
+                "inner_1",
+            ),
+            Project(
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.13"
+                """,
+                """
+                [tool.poetry.dependencies]
+                python = "^3.10"
+                shpyx = "0.0.14"
+                """,
+                "inner_2",
+            ),
+        ],
+        tmp_dir=tmp_dir.name,
+    )
+
+    tmp_dir.cleanup()
+
+    # Verify that the updates were called in the correct order.
+    cmd_runs = [(mock_call.args[0], Path(mock_call.kwargs["exec_dir"]).name) for mock_call in shpyx_run_spy.mock_calls]
+    assert cmd_runs == [
+        ("poetry update --lock", "inner_2"),
+        ("poetry show -o --no-ansi", "inner_2"),
+        ("poetry update --lock", "inner_2"),
+        ("poetry update --lock", "inner_1"),
+        ("poetry show -o --no-ansi", "inner_1"),
+        ("poetry update --lock", "outer"),
+        ("poetry show -o --no-ansi", "outer"),
+    ]
